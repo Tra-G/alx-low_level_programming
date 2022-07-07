6385e07d00d80436afa6f69447335aaa9b494628
Tracy <okorietracy8@gmail.com>
2022-07-06 19:20:53 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - first 50 fibonacci number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int n1 = 1, result, n2 = 2, nLast = 50;
+	int ii;
+
+	printf("%lu, %lu, ", n1, n2);
+	for (ii = 2; ii < nLast; ii++)
+	{
+		result = n1 + n2;
+		n1 = n2;
+		n2 = result;
+		printf("%lu", result);
+		if (ii != nLast - 1)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
+
