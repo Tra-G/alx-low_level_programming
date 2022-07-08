7ca4d82e2eef9294106f3fbcfa85d7d7979b2f04
Tracy <okorietracy8@gmail.com>
2022-07-08 10:32:09 -0700

    task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the largest prime factor of an int
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int main(void)
+{
+	long int var = 612852475143;
+	long int primeFact = 2;
+
+	while (var > 1)
+	{
+		if (var % primeFact == 0)
+			var /= primeFact;
+		else
+			primeFact++;
+	}
+	printf("%ld\n", primeFact);
+	return (0);
+}
